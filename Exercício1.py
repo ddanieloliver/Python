5bafe45b433da9ba4ac473359726c2a6ba8f91ef
Daniel Vitor Oliveira Nascimento <92951925+ddanieloliver@users.noreply.github.com>
2022-04-12 17:34:36 -0300

    Primeiro exercicio da facul

---
+++ Exercício1.py
@@ -0,0 +1,19 @@
+'''
+1) Encontre os identificadores inválidos abaixo. Justifique.
+$salario (F)
+salario$ (V)
+a[1] (F)
+xKH (V)
+alfa 2 (F)
+2vizinhos (F)
+val0r (V)
+valOr (V)
+a>b (F)
+a+b (F)
+qwert (V)
+guarda_chuva (V)
+U.F. (F)
+_o (V)
+DiaAdia (V) 
+bestaTeste (V)
+''' 
