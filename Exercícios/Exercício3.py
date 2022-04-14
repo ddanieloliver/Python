390b3b2cbceec708323b7ebe1248432e1f0e4b10
Daniel Vitor Oliveira Nascimento <92951925+ddanieloliver@users.noreply.github.com>
2022-04-14 14:45:56 -0300

    pass

---
+++ Exercícios/Exercício3.py
@@ -0,0 +1,48 @@
+'''3) Suponha que o símbolo ÷ divide dois números e retorna o número inteiro resultado da divisão
+sem a parte fracionária e o símbolo / divide dois números e retorna um valor real com a resposta
+exata. Ambos os operadores possuem a mesma precedência. Sabendo que os valores das variáveis
+são X = – 1, Y = 3 e Z = 7, calcule os resultados das seguintes atribuições.
+a) Y ← Y +1 
+b) Y ← Y +3 
+c) Media ← (X + Y + Z) / 3 
+d) Media ← X + Y + Z / 3
+e) K←Z÷Y /3
+f) K←(Z÷Y )/3
+g) K←Z÷(Y /3)'''
+
+X = -1 
+Y = 3
+Z = 7
+
+Y = Y + 1
+print(Y)
+
+X = -1 
+Y = 3
+Z = 7
+
+Y = Y + 3
+print (Y)
+
+X = -1 
+Y = 3
+Z = 7
+
+Media = (X + Y + Z)/ 3
+print(Media)
+
+X = -1 
+Y = 3
+Z = 7
+
+Media = X + Y + Z/3
+print(Media)
+
+K = Z // Y / 3
+print(K)
+
+K = (Z // Y) / 3
+print(K)
+
+K = Z // (Y / 3)
+print(K)
