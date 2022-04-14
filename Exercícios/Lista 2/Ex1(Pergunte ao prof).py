967c0a23103dfbc3cf236de34632df92c988a83f
Daniel Vitor Oliveira Nascimento <92951925+ddanieloliver@users.noreply.github.com>
2022-04-14 15:30:52 -0300

    Continundo os trabalhos

--- Exercícios/Lista 2/Ex1(Pergunte ao prof).py
+++ Exercícios/Lista 2/Ex1(Pergunte ao prof).py
@@ -3,17 +3,16 @@
 deve ser capaz de informar isto.
 '''
 
-numero1 = (input('insira o primeiro número: '))
-numero2 = (input('insira o segundo número: '))
-if numero1.isnumeric() == True and numero2.isnumeric() == True: 
+n1 = float(input('insira o primeiro número: '))
+n2 = float(input('insira o segundo número: '))
+if n1 == n2:
+    print('Os números são iguais')
 
-    if numero1 == numero2:
-        print( 'Os números são iguais')
-    elif numero1 > numero2:
-        print( f'O numero {numero1} é maior')
-    else:
-        print( f'O numero {numero2} é maior')
+elif n1 < n2:
+    print(f'O numero {n2} é maior')
+
+elif n1 > n2:
+    print(f'O numero {n1} é maior')
+
 else:
-    print('insira um número válido')
-'''A validação não ocorre da forma correta,
- não está indentificando números menores/'''
+    print('Insira um valor válido')
