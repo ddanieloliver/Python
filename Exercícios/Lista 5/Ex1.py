a06bf7259245f0284f9f08c5b5be050215b62237
Daniel Vitor Oliveira Nascimento <dnielvitoroliveira456@gmail.com>
2022-07-02 18:40:48 -0300

    usei slice e for

---
+++ Exercícios/Lista 5/Ex1.py
@@ -0,0 +1,19 @@
+'''1) Escreva uma função inverte(s) que recebe uma string s e retorna a os caracteres da string de
+trás para frente. Não use a função reverse da linguagem Python. Ex:
+inverte("Russas") "sassuR" '''
+
+
+
+'''def inverte(string):
+    s_invertida = ""
+    for i in range (len(string)):
+        s_invertida = s_invertida + string[len(string) -i -1]    
+    print ( s_invertida)
+s = str(input( 'insira a string que deseja inverter: '))
+inverte(s)
+'''
+
+def inverte(string):
+    return string[::-1]
+
+print(inverte('Russas'))
