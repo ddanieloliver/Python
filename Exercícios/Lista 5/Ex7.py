46fd1e573a8cbf271fbd27652cf934cfdd251a92
Daniel Vitor Oliveira Nascimento <dnielvitoroliveira456@gmail.com>
2022-07-03 19:04:47 -0300

    Fácil

---
+++ Exercícios/Lista 5/Ex7.py
@@ -0,0 +1,9 @@
+'''7) Escreva um programa que armazene 10 números inteiros lidos do usuário em uma lista e depois
+escreva os valores armazenados.'''
+
+lista = []
+for i in range(11):
+    adiciona = input("Insira algo: ")
+    lista.append(adiciona)
+
+print(lista)
