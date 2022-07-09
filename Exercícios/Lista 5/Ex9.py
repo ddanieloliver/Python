d599c9fb7cf98eaae84e5ece9c4de4b28ff6311a
Daniel Vitor Oliveira Nascimento <dnielvitoroliveira456@gmail.com>
2022-07-09 17:21:08 -0300

    Usando laços

---
+++ Exercícios/Lista 5/Ex9.py
@@ -0,0 +1,7 @@
+'''9) Crie uma função numerosAleatorios(n, min, max) que retorna uma lista com n números
+inteiros entre os valores min (incluso) e max (não incluso). Ex:
+numerosAleatorios(5,10,30) [13,29,23,13,21]'''
+
+
+def numerosAleatorios(n, min, max):
+    
